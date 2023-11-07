efb9853afbf79312513ec7185ec7e48480f7f12f
Luigi-Iorio <luigi0iorio@gmail.com>
2023-11-07 17:43:05 +0100

    fix: parametro stringa

--- js/main.js
+++ js/main.js
@@ -1,24 +1,25 @@
 "use strict";
 
 // Nome - Cognome del passeggero
-// const nomeCognome = document.getElementById(nameSurname);
+const nomeCognome = document.getElementById("nameSurname");
 
-// nomeCognome.addEventListener("keypress", function (e) {
-//   console.log(e.key);
-// });
-
-// Km da percorrere
-const numeroKm = document.getElementById(kmDaPercorrere);
-numeroKm.addEventListener("keypress", function (e) {
+nomeCognome.addEventListener("keypress", function (e) {
   console.log(e.key);
 });
 
-// Età del passeggero
-const etaPasseggero = document.getElementById(etaPasseggero);
+// Km da percorrere
+let numeroKm = document.getElementById("kmDaPercorrere");
+numeroKm.addEventListener("keypress", function (e) {
+  console.log(e.key);
+  numeroKm = parseFloat(e.key);
+});
 
-const maggiorenne = document.getElementById(maggiorenne);
-const minorenne = document.getElementById(minorenne);
-const over = document.getElementById(over);
+// Età del passeggero
+const etaPasseggero = document.getElementById("etaPasseggero");
+
+const maggiorenne = document.getElementById("maggiorenne");
+const minorenne = document.getElementById("minorenne");
+const over = document.getElementById("over");
 
 // Calcolo prezzo totale del biglietto
 let prezzoTotale = numeroKm * 0.21; //prezzo predefinito
@@ -34,7 +35,7 @@ if (etaPasseggero == minorenne) {
 }
 
 // Visualizzazione prezzo in console al click del bottone
-const genera = document.getElementById(genera);
+const genera = document.getElementById("genera");
 
 genera.addEventListener("click", function () {
   console.log(messaggio, parseFloat(prezzoTotale.toFixed(2)));
