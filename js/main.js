c443491cd2519a497d0281d6077e7bdfdab7e52a
Luigi-Iorio <luigi0iorio@gmail.com>
2023-11-07 18:30:16 +0100

    fix: valori e calcoli dopo funzione per il click

--- js/main.js
+++ js/main.js
@@ -1,42 +1,28 @@
 "use strict";
 
-// Nome - Cognome del passeggero
-const nomeCognome = document.getElementById("nameSurname");
-
-nomeCognome.addEventListener("keypress", function (e) {
-  console.log(e.key);
-});
-
-// Km da percorrere
-let numeroKm = document.getElementById("kmDaPercorrere");
-numeroKm.addEventListener("keypress", function (e) {
-  console.log(e.key);
-  numeroKm = parseFloat(e.key);
-});
-
-// Età del passeggero
-const etaPasseggero = document.getElementById("etaPasseggero");
-
-const maggiorenne = document.getElementById("maggiorenne");
-const minorenne = document.getElementById("minorenne");
-const over = document.getElementById("over");
-
-// Calcolo prezzo totale del biglietto
-let prezzoTotale = numeroKm * 0.21; //prezzo predefinito
-let messaggio = "Il prezzo totale del biglietto è: "; //messaggio predefinito
-
-// Calcolo prezzo scontato
-if (etaPasseggero == minorenne) {
-  prezzoTotale = prezzoTotale - (prezzoTotale * 20) / 100;
-  messaggio = "Il prezzo scontato del 20% è: ";
-} else if (etaPasseggero == over) {
-  prezzoTotale = prezzoTotale - (prezzoTotale * 40) / 100;
-  messaggio = "Il prezzo scontato del 40% è: ";
-}
-
-// Visualizzazione prezzo in console al click del bottone
-const genera = document.getElementById("genera");
-
 genera.addEventListener("click", function () {
+  // Numero di km
+  let numeroKm = parseFloat(document.getElementById("kmDaPercorrere").value);
+
+  // Età passeggero
+  const etaPasseggero = document.getElementById("etaPasseggero").value;
+
+  let minorenne = document.getElementById("minorenne").value;
+  let over = document.getElementById("over").value;
+
+  // prezzo totale
+  let prezzoTotale = numeroKm * 0.21; //prezzo predefinito
+  let messaggio = "Il prezzo totale del biglietto è: "; //messaggio predefinito
+
+  // Calcolo per lo sconto
+  if (etaPasseggero === minorenne) {
+    prezzoTotale = prezzoTotale - (prezzoTotale * 20) / 100;
+    messaggio = "Il prezzo scontato del 20% è: ";
+  } else if (etaPasseggero === over) {
+    prezzoTotale = prezzoTotale - (prezzoTotale * 40) / 100;
+    messaggio = "Il prezzo scontato del 40% è: ";
+  }
+
+  // visualizzazione in console del prezzo
   console.log(messaggio, parseFloat(prezzoTotale.toFixed(2)));
 });
